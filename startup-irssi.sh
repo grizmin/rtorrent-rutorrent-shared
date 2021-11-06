6f0a0a13aa031aad1f6e35cf701cd285da0179f7
Konstantin Krastev <kkonstantin@vmware.com>
2021-11-07 00:34:52 +0200

    fix autodl

--- startup-irssi.sh
+++ startup-irssi.sh
@@ -51,8 +51,8 @@ then
 	# unzip -o autodl-irssi.zip >/dev/null 2>&1
 	# rm autodl-irssi.zip
 	git clone https://github.com/hiphopotamus/autodl-irssi
-	cd autodl-irssi
-	cp autodl-irssi.pl autorun/
+	cp -rp AutodlIrssi ../
+	cp autodl-irssi/autodl-irssi.pl autorun/
 	chown -R rtorrent:rtorrent /home/rtorrent/.irssi
 	sed -i -e 's/1.86/1.84/g' /home/rtorrent/.irssi/scripts/AutodlIrssi/SslSocket.pm
 else
