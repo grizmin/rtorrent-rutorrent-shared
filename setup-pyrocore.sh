94caecb991006d886d7adc43b4b93613336071a9
Konstantin Krastev <kkonstantin@vmware.com>
2021-11-10 23:42:30 +0200

    pyrocore install v2

--- setup-pyrocore.sh
+++ setup-pyrocore.sh
@@ -2,13 +2,14 @@
 
 #check if pyrocore was installed.
 if [[ ! -x /home/rtorrent/bin/pyroadmin ]]; then
-    echo "rutorrent ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    sudo su - rtorrent
-    mkdir -p ~/bin ~/.local
-    git clone "https://github.com/pyroscope/pyrocore.git" ~/.local/pyroscope
-    ~/.local/pyroscope/update-to-head.sh
-    echo 'export PATH=$PATH:/home/rtorrent/bin' >> ~/.bashrc
-    pyroadmin --version
+    echo "rtorrent ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    exec sudo -u rtorrent /bin/bash - << EOF
+        mkdir -p ~/bin ~/.local
+        git clone "https://github.com/pyroscope/pyrocore.git" ~/.local/pyroscope
+        ~/.local/pyroscope/update-to-head.sh
+        echo 'export PATH=$PATH:/home/rtorrent/bin' >> ~/.bashrc
+        pyroadmin --version
+EOF
 else
     echo "Pyrocore found."
 fi
